ddd2ab66b80dbf38c3fa96448c444f64c3734fd3
ghornos <ghornos@fintech-consulting.services>
2023-12-22 12:12:26 -0300

    init project

---
+++ src/main/java/com/hornosg/pricesnewsletter/PricesNewsletterServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hornosg.pricesnewsletter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PricesNewsletterServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PricesNewsletterServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hornosg/pricesnewsletter/PricesNewsletterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hornosg.pricesnewsletter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PricesNewsletterServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
